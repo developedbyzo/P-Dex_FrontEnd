dc270302257e2e4ed878a40882838a6f21f6c3de
Alonzo Moses <alonzo.k.moses@gmail.com>
2018-01-23 11:27:11 -0500

    setting up link to backend

--- pokemern-dex/src/components/Pokemon/Pokemon.js
+++ pokemern-dex/src/components/Pokemon/Pokemon.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react'
-
+import pokeRequest from '../../request'
 class Pokemon extends Component {
+  state = {
+    pokemon: []
+  }
   render () {
+    componentDidMount = () => {
+      pokeRequest.then(res => this.setState({
+        pokemon: res.data
+      }))
+    }
     return (
-      
+      <div className='row'>
+        <div className='card-panel hoverable center col s1'>
+          <img className='responsive-img' src='${}' />
+        </div>
+      </div>
     )
   }
 }
---
+++ pokemern-dex/src/constant.js
@@ -0,0 +1,1 @@
+export const POKEMERN_URL = 'http://localhost:6009/pokies'
